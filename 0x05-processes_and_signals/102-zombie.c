87d3b9cefa023e2e675108df93639c57d109df95
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-26 01:38:43 -0800

    test

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t child_pid;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        child_pid = fork();
+        if (child_pid > 0)
+        {
+            printf("Zombie process created, PID: %d\n", child_pid);
+            sleep(1);
+        }
+        else if (child_pid == 0)
+        {
+            exit(0);
+        }
+        else
+        {
+            perror("fork");
+            return (1);
+        }
+    }
+
+    infinite_while();
+
+    return (0);
+}
