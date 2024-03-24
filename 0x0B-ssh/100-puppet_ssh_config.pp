0f7e7905393bb065ad14c2af2fe0de1140c88151
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-24 01:33:31 -0700

    test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,12 +3,12 @@ include stdlib
 
 # Ensure SSH client configuration for passwordless authentication
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
 
