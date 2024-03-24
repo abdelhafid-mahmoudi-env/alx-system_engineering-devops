356c260ef9e41e0d89b0a3be573e0c83264bc574
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-23 22:41:11 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-# Puppet manifest to kill a process named "killmenow"
-
-exec { 'killmenow':
-  command     => 'pkill -9 -f killmenow',
-  path        => ['/usr/bin', '/usr/sbin', '/bin'],
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+# execute pkill command to kill bashscript file killmenow
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
