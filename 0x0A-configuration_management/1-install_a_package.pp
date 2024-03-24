6ee77540173a6a65487945df37fc3bc100a12446
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-23 23:51:51 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Puppet manifest to install Flask package
+
+package { 'Werkzeug':
+    ensure   => '2.2.2',
+    provider => 'pip3'
+}
+
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
 
