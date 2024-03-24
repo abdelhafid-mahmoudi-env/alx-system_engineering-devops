cc1b10310f179b6b96c97e435b9287eb235cd437
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-23 23:33:27 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # Using Puppet, install flask from pip3.
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install flask':
+  command =>  'pip3 install flask==2.1.0',
+  path    =>  '/usr/bin/',
+  unless  =>  'pip3 list | grep flask',
 }
 
