0bbae38a9a5f9dd538877c6325abc8996efaac3c
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-23 22:55:54 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# Puppet manifest to kill a process named "killmenow"
-exec { 'killmenow':
-  command     => 'pkill -9 -f killmenow',
-  path        => ['/usr/bin', '/usr/sbin', '/bin'],
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+# Puppet manifest to install Flask package
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
