8cb3dc91fa76b08e848337f2b53405acf932599d
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-22 16:51:45 -0700

    test

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'
-    url = url.format(user_id)
-    response = requests.get(url)
-    tasks = response.json()
 
-    # Write data to CSV
-    filename = '{}.csv'.format(user_id)
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = [
-            'USER_ID', 'USERNAME',
-            'TASK_COMPLETED_STATUS', 'TASK_TITLE'
-        ]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for task in tasks:
-            writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': task.get('username'),
-                'TASK_COMPLETED_STATUS': task.get('completed'),
-                'TASK_TITLE': task.get('title')
-            })
+def export_todo_list(user_id):
+    """
+    Exports to-do list information for a given employee ID to CSV format.
+
+    Args:
+        user_id (str): The ID of the user whose to-do list is to be exported.
+
+    Returns:
+        None
+    """
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Fetching user data and to-dos
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    # Writing to CSV
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        # Writing CSV header
+        header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer.writerow(header)
+        # Writing to CSV row by row
+        for todo in todos:
+            row_data = [
+                user_id,
+                username,
+                todo.get("completed"),
+                todo.get("title")
+            ]
+            writer.writerow(row_data)
+
+
+if __name__ == "__main__":
+    # Fetching user ID from command line arguments
+    user_id = sys.argv[1]
+    export_todo_list(user_id)
