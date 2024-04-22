4437956d0db6b4673fa39e0f2bf41ff1b02bf145
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-22 16:54:46 -0700

    test

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+""" Script to export data into a CSV format """
 import csv
 import requests
 import sys
 
-
-def export_todo_list(user_id):
-    """
-    Exports to-do list information for a given employee ID to CSV format.
-
-    Args:
-        user_id (str): The ID of the user whose to-do list is to be exported.
-
-    Returns:
-        None
-    """
+if __name__ == "__main__":
+    # Base URL for the API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetching user data and to-dos
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    # Extract user ID from command line argument
+    uid = sys.argv[1]
 
-    # Writing to CSV
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        # Writing CSV header
-        header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer.writerow(header)
-        # Writing to CSV row by row
-        for todo in todos:
-            row_data = [
-                user_id,
-                username,
-                todo.get("completed"),
-                todo.get("title")
-            ]
-            writer.writerow(row_data)
+    # Retrieve user data
+    u_resp = requests.get(url + "users/{}".format(uid))
+    u_data = u_resp.json()
+    uname = u_data.get("username")
 
+    # Retrieve todo data for the user
+    t_resp = requests.get(url + "todos", params={"userId": uid})
+    t_data = t_resp.json()
 
-if __name__ == "__main__":
-    # Fetching user ID from command line arguments
-    user_id = sys.argv[1]
-    export_todo_list(user_id)
+    # Write data to a CSV file
+    with open("{}.csv".format(uid), "w", newline="") as c_file:
+        c_writer = csv.writer(c_file, quoting=csv.QUOTE_ALL)
+        for task in t_data:
+            c_writer.writerow([
+                uid,
+                uname,
+                task.get("completed"),
+                task.get("title")
+            ])
