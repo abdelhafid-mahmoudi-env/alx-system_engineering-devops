281311293eb0ed77f26cbb8e81baaa6e72d8c12b
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-22 16:46:50 -0700

    test

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-'''A script that gathers employee name completed
-tasks and total number of tasks from an API
-'''
-
-import re
+import csv
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+    response = requests.get(url)
+    tasks = response.json()
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            employee_id = int(sys.argv[1])
-            emp_req = requests.get(f'{REST_API}/users/{employee_id}').json()
-            task_req = requests.get(f'{REST_API}/todos').json()
-            emp_name = emp_req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == employee_id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):'
-            )
-            for task in completed_tasks:
-                print(f'\t{task.get("title")}')
+    # Write data to CSV
+    filename = '{}.csv'.format(user_id)
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for task in tasks:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': task.get('username'),
+                'TASK_COMPLETED_STATUS': task.get('completed'),
+                'TASK_TITLE': task.get('title')
+            })
