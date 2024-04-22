97401a0a43bdf610b5822d1a46df675b3cf055bb
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-22 16:45:06 -0700

    test

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""
-Gather data from an API and export to CSV
-"""
+'''A script that gathers employee name completed
+tasks and total number of tasks from an API
+'''
+
+import re
 import requests
-import csv
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    user_id = argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user_id
-    response_user = requests.get(url_user)
-    user_data = response_user.json()
+REST_API = "https://jsonplaceholder.typicode.com"
 
-    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId=' + user_id
-    response_todos = requests.get(url_todos)
-    todos_data = response_todos.json()
-
-    filename = user_id + ".csv"
-
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([
-            "USER_ID",
-            "USERNAME",
-            "TASK_COMPLETED_STATUS",
-            "TASK_TITLE"
-        ])
-        for task in todos_data:
-            writer.writerow([
-                user_id,
-                user_data['username'],
-                task['completed'],
-                task['title']
-            ])
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            employee_id = int(sys.argv[1])
+            emp_req = requests.get(f'{REST_API}/users/{employee_id}').json()
+            task_req = requests.get(f'{REST_API}/todos').json()
+            emp_name = emp_req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == employee_id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):'
+            )
+            for task in completed_tasks:
+                print(f'\t{task.get("title")}')
