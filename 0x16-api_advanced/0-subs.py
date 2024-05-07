eb345e885200d13deb6483129be58ab37dabeabf
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-07 15:59:37 -0700

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns the number of subscribers
-for a given subreddit.
+Contains the number_of_subscribers function,
+which queries the Reddit API and retrieves
+the number of subscribers for a given subreddit.
 """
-
 import requests
+import sys
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyBot/0.0.1"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Queries the Reddit API for the number of subscribers
+    and returns the count.
+    """
+    u_agent = 'Mozilla/5.0'
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if res.status_code != 200:
         return 0
+    
+    dic = res.json()
+    
+    if 'data' not in dic:
+        return 0
+    
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    
+    return res.json()['data']['subscribers']
