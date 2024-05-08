1325578fb3c758ec658afd9d49855ca8f124afa4
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-07 18:04:12 -0700

    test

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,38 +7,50 @@ import requests
 
 def count_words(subreddit, word_list, after=None, word_counts={}):
     """
-    Recursive function to count occurrences of keywords in hot articles of a subreddit
+    Recursive function to count occurrences.
     """
     if not word_list:
-        # Base case: when there are no more keywords to count
-        sorted_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+        sorted_counts = sorted(
+                word_counts.items(),
+                key=lambda x: (-x[1], x[0])
+        )
         for word, count in sorted_counts:
             print("{}: {}".format(word, count))
         return
 
-    word = word_list.pop(0).lower()  # Get and remove the first word from the list
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    word = word_list.pop(0).lower()
+    pathname = "https://www.reddit.com/r/{}/hot.json?limit=100"
+    url = pathname.format(subreddit)
     headers = {"User-Agent": "reddit-{}".format(subreddit)}
     params = {"after": after} if after else None
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+    )
 
     if response.status_code == 200:
         data = response.json().get("data")
         if data and data.get("children"):
             for post in data.get("children"):
                 title = post.get("data").get("title").lower()
-                word_counts[word] = word_counts.get(word, 0) + title.count(word)
+                wrding = word_counts.get(word, 0)
+                titling = title.count(word)
+                word_counts[word] = wrding + titling
             after = data.get("after")
             if after:
                 return count_words(subreddit, word_list, after, word_counts)
             else:
                 return count_words(subreddit, word_list, None, word_counts)
 
-    # No matches or subreddit is invalid
     if word_list:
         count_words(subreddit, word_list, None, word_counts)
     else:
-        sorted_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+        sorted_counts = sorted(
+                word_counts.items(),
+                key=lambda x: (-x[1], x[0])
+        )
         for word, count in sorted_counts:
             print("{}: {}".format(word, count))
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,13 +7,19 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """
-    Returns a list containing the titles of all hot articles for a given subreddit
+    Returns a list containing the titles of all hot articles.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    pathname = "https://www.reddit.com/r/{}/hot.json?limit=100"
+    url = pathname.format(subreddit)
     headers = {"User-Agent": "reddit-{}".format(subreddit)}
     params = {"after": after} if after else None
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+    )
 
     if response.status_code == 200:
         data = response.json().get("data")
