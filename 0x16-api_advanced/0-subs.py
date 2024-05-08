6722dc3f3f71d9d45ea522cee760296c88ba6469
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-07 18:31:02 -0700

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,10 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "reddit-{}".format(subreddit)}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json().get("data")
-        if data:
-            return data.get("subscribers", 0)
-    return 0
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
